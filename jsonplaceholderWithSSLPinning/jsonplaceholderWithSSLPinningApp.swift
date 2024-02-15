4b4d08fc4c2ebbfeb4f920a22a8b421224bd6702
lulu <lulukalmisfer@hotmail.com>
2024-02-15 11:11:33 +0300

    Initial Commit

---
+++ jsonplaceholderWithSSLPinning/jsonplaceholderWithSSLPinningApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonplaceholderWithSSLPinningApp.swift
+//  jsonplaceholderWithSSLPinning
+//
+//  Created by Lulwah Almisfer on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct jsonplaceholderWithSSLPinningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
